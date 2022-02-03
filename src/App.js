0360e9558c1a9bc7099b37419c397e7157484813
Isaias Palacio <alemais2003312@gmail.com>
2022-02-03 15:45:40 -0500

    Second

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
+import React, { useState } from 'react';
+
 import './styles/style.css';
 import Card from './components/Card';
 
 const {kings} = require('./data/data.json');
 
 function App() {
+
+    const [king, setKing] = useState(kings[0]),
+    
+    [data, setData] = useState(1),
+
+    [info, setInfo] = useState();
+
+    function changeText(e){
+        if (data < 10){
+            if ( data == 7){
+                e.target.classList.add("red-text")
+            } else {
+                e.target.classList.remove("red-text")
+            }
+            setData(data + 1)
+        }else{
+            setData( 1 );
+        }
+    }
+
+    function readText(e){
+        e.preventDefault();
+        setData(info);
+    }
+
+    function changeImg(e){
+        if( king.id < kings.length ){
+            setKing(kings[king.id]);
+        }else if( king.id == 3 ){
+            setKing( kings[0] );
+        }
+    }
+
     return (
         <div className="container">
             <div className="row">
-                    {
-                        kings.map((card) => {
-                            return (
-                                <div className="col-sm">
-                                    <Card
-                                        img={card['img-src']}
-                                        title={card['title']}
-                                    />
-                                </div>
-                            )
-                        })
-                    }
+                <form>
+                    <div className='text'>
+                        <span className='text-info' onClick={changeText}>
+                            { data ? data : "No hay contenido"}
+                        </span>
+                    </div>
+
+                    <div className='button'>
+                        <button className='btn btn-primary' onClick={readText}>
+                            Enviar
+                        </button>
+                    </div>
+                    
+                    <div className="col-sm" onClick={changeImg}>
+                        <Card
+                            img={king['img-src']}
+                            title={king['title']}
+                        />
+                    </div>
+                    
+                    <div className='input'>
+                        <input type="text" name="name" onChange={(e) => setInfo(e.target.value)}/>
+                    </div>
+                </form>
             </div>
         </div>
   );
